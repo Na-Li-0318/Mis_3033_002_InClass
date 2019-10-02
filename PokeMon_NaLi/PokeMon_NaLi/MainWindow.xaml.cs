53c18a3da5770642d99663069aae6061862cad8b
nancy.nali1995@gmail.com <nancy.nali1995@gmail.com>
2019-10-02 11:25:29 -0500

    pokemon

---
+++ PokeMon_NaLi/PokeMon_NaLi/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿//Na Li
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PokeMon_NaLi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        ////https://got-quotes.herokuapp.com/quotes
+        //    using (HttpClient client = new HttpClient())
+        //    {
+        //        var response = client.GetAsync(@"https://pokeapi.co/api/v2/pokemon?limit=964").Result;
+        //        if (response.IsSuccessStatusCode)
+        //        {
+        //            var content = response.Content.ReadAsStringAsync().Result;
+        //            var VARIABLE = JsonConvert.DeserializeObject<Type>(content);
+        //            var x = JsonConvert.SerializeObject(VARIABLE);
+        //        }
+        //    }
+        }
+
+        private void ChooseBT_Click(object sender, RoutedEventArgs e)
+        {
+            //https://pokeapi.co/api/v2/pokemon?limit=964
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = client.GetAsync(@"https://pokeapi.co/api/v2/pokemon?limit=964").Result;
+                if(response.IsSuccessStatusCode)
+                {
+                    var content = response.Content.ReadAsStringAsync().Result;
+                    //ChooseBT gq = JsonConvert.DeserializeObject<ChooseBT>(content);
+                    var stuff= JsonConvert.DeserializeObject<Results>(content);
+                    // pokemonlist.Text = stuff.quote + stuff.charters;
+                    foreach (var result in stuff.results)
+                    {
+                     lstPokes.Items.Add(result.name);
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ PokeMon_NaLi/PokeMon_NaLi/pokemon.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeMon_NaLi
+{
+    class pokemon
+    {
+        public int height { get; set; }
+        public int weight { get; set; }
+        public string name { get; set; }
+
+        public List<Result> results { get; set; }
+
+   
+
+        public override string ToString()
+        {
+            return $"{name} is {height} in height, and {weight} in weight";
+        }
+    }
+}
